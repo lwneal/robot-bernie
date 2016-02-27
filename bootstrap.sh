8ab147ecd900b64b687d557d4e03100996576fd5
Lawrence Neal <nealla@lwneal.com>
2016-02-27 12:05:05 -0800

    Add requirements bootstrap file for Ubuntu AWS

---
+++ bootstrap.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+set -eux
+
+sudo aptitude update
+
+sudo DEBIAN_FRONTEND=noninteractive aptitude install -y -o Dpkg::Options::="--force-confnew" -o Dpkg::Options::="--force-confold" \
+    build-essential \
+    openssh-server \
+    emacs24-nox \
+    wget \
+    libglu-dev \
+    linux-headers-`uname -r` \
+    linux-image-`uname -r` \
+    linux-image-extra-`uname -r` \
+    python-pip \
+    python-dev \
+    gfortran \
+    python-matplotlib \
+    libblas-dev \
+    liblapack-dev \
+    uuid-dev \
+    libzmq-dev \
+    python-nose \
+    git \
+
+export CUDA_BUILD=/mnt/cuda
+export CUDA_HOME=/opt/cuda
+sudo mkdir -p $CUDA_BUILD
+sudo chmod 777 $CUDA_BUILD
+pushd $CUDA_BUILD
+sudo wget http://developer.download.nvidia.com/compute/cuda/6_0/rel/installers/cuda_6.0.37_linux_64.run
+sudo chmod +x cuda_6.0.37_linux_64.run
+sudo ./cuda_6.0.37_linux_64.run \
+    -silent \
+    -driver \
+    -toolkit \
+    -toolkitpath=$CUDA_HOME/toolkit \
+    -samples \
+    -samplespath=$CUDA_HOME/samples \
+    -verbose \
+    -override \
+    -kernel-source-path=/usr/src/linux-headers-`uname -r`
+popd
+
+sudo update-initramfs -u
+
+sudo ldconfig
+
+sudo pip install \
+    pylint==0.28.0 \
+    ipython[all]==2.4.0 \
+    theano
+
+sudo apt-get install python-h5py
+sudo pip install keras
