ab08de52df6836b5be41e559c241db4ecdd00364
Lawrence Neal <nealla@lwneal.com>
2016-04-19 00:43:12 -0700

    Fix lint

--- webapi.py
+++ webapi.py
@@ -4,6 +4,7 @@ import bernie
 app = flask.Flask(__name__, static_url_path='/static/')
 model = bernie.load_model()
 
+
 @app.route('/ask_question')
 def ask_question():
     question = flask.request.args.get('question')
@@ -12,13 +13,16 @@ def ask_question():
     print 'Returning answer: {}'.format(answer)
     return answer
 
+
 @app.route('/')
 def route_index():
     return app.send_static_file('bernie.html')
 
+
 @app.route('/<path:path>')
 def send_static_file(path):
     return flask.send_from_directory('static', path)
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=8000)
