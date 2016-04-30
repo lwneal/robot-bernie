e59a60c9199801a820f49c3fd409c93435e302ad
Larry Neal <nealla@lwneal.com>
2016-04-29 19:07:32 -0700

    Cleanup and refactor

--- train_bernie.py
+++ train_bernie.py
@@ -7,6 +7,7 @@ import numpy as np
 import sys
 import random
 import sys
+import os
 import re
 
 BATCH_SIZE = 128
@@ -22,46 +23,68 @@ def make_char_lookup_table(text):
     indices_char = dict((i, c) for i, c in enumerate(chars))
     return char_indices, indices_char
 
-def read_training_batch(text, char_indices, batch_size=BATCH_SIZE):
-    # Read characters from the text file
-    start = random.randint(0, len(text) - batch_size - 1)
-    end = start + batch_size + 1
-    characters = text[start:end]
-    # One-hot encode them
-    char_count = len(char_indices)
-    X = np.zeros((batch_size, 1, char_count), dtype=np.bool)
-    y = np.zeros((batch_size, char_count), dtype=np.bool)
-    for i in range(batch_size):
-        X[i, 0, char_indices[characters[i]]] = 1
-        y[i, char_indices[characters[i + 1]]] = 1
-    return X, y
+def generate_text_stream(filename, offset=0):
+    with open(filename) as fp:
+        fp.seek(offset)
+        while True:
+            val = fp.read(1)
+            # Remove punctuation and lowercase all letters
+            val = re.sub(r'[^a-zA-Z0-9\.\n]+', ' ', val).lower()
+            if not val:
+                print("Text stream with offset {} hit end of document, seeking back to start".format(offset))
+                fp.seek(0)
+                continue
+            yield val
 
-def generate_training_data(text, char_indices, batch_size=BATCH_SIZE):
-    X = np.zeros((batch_size, 1, len(char_indices)))
-    y = np.zeros((batch_size, len(char_indices)))
-    # TODO: 128 streams from different positions in the file, not all the same at once
+def onehot_encode(generator, char_indices):
+    char_count = len(char_indices)
+    for val in generator:
+        idx = char_indices[val]
+        v = np.zeros(char_count)
+        v[idx] = 1
+        yield v
+
+def generate_training_data(filename, char_indices, batch_size=BATCH_SIZE):
+    corpus_len = os.path.getsize(filename)
+    char_count = len(char_indices)
+    X = np.zeros((batch_size, 1, char_count))
+    y = np.zeros((batch_size, char_count))
+
+    generators = []
     for i in range(batch_size):
-        X[i, 0, char_indices[text[i]]] = 1
-        y[i, char_indices[text[i]]] = 1
+        offset = random.randint(0, corpus_len)
+        g = onehot_encode(generate_text_stream(filename, offset), char_indices)
+        generators.append(g)
+
+    for i in range(batch_size):
+        X[i] = next(generators[i]).reshape(X[i].shape)
+
+    indices_char = {v:k for (k,v) in char_indices.items()}
+    def np_to_char(x):
+        if not x.any():
+            return '?'
+        idx = np.nonzero(x)[0][0]
+        return indices_char[idx].replace('\n', 'NEWLINE')
+    
     while True:
-        for i in range(batch_size, len(text)):
-            y = np.roll(y, -1, axis=0)
-            y[batch_size - 1, :] = 0
-            y[batch_size - 1, char_indices[text[i]]] = 1
-            if (i % 1000 == 0):
-                print("learning from idx {}".format(i))
-            yield (X, y)
-            X = y.reshape(X.shape)
+        doubles = 0
+        for i in range(batch_size):
+            y[i] = next(generators[i])
+            a, b = np_to_char(X[i][0]), np_to_char(y[i])
+            if a == b:
+                doubles += 1
+        yield (X, y)
+        X[:] = y.reshape(X.shape)
 
 def build_model(char_count, batch_size=BATCH_SIZE):
     model = Sequential()
     model.add(LSTM(512, return_sequences=True, batch_input_shape=(batch_size, 1, char_count), stateful=True))
-    model.add(Dropout(0.2))
+    #model.add(Dropout(0.2))
     model.add(LSTM(512, return_sequences=False, stateful=True))
-    model.add(Dropout(0.2))
+    #model.add(Dropout(0.2))
     model.add(Dense(char_count))
     model.add(Activation('softmax'))
-    optimizer = RMSprop(lr=.0001)
+    optimizer = RMSprop(lr=.001)
     model.compile(loss='categorical_crossentropy', optimizer=optimizer)
     return model
 
@@ -80,7 +103,7 @@ def predict(model, current_char, char_indices, indices_to_char, batch_size=BATCH
     return indices_to_char[char_idx]
 
 
-def main(run_name, text):
+def main(run_name, corpus_filename, text):
     chars = set(text)
     print('Found {} distinct characters: {}'.format(len(chars), ''.join(chars)))
     char_indices, indices_char = make_char_lookup_table(text)
@@ -97,38 +120,22 @@ def main(run_name, text):
         characters = [np_to_char(x) for x in array]
         return ''.join(characters).replace('\n', '\\N')
 
-    def print_prediction(model, char_indices, indices_char):
+    # train the model, output generated text after each iteration
+    generator = generate_training_data(corpus_filename, char_indices)
+    for iteration in range(1, 1000):
+        print('-' * 50)
+        print('Iteration {}'.format(iteration))
+        model.reset_states()
+        i = 0
+        model.fit_generator(generator, samples_per_epoch=2**18, nb_epoch=1, verbose=1)
+
+        model.reset_states()
         next_char = random.choice(char_indices.keys())
         for i in range(512):
             next_char = predict(model, next_char, char_indices, indices_char)
             sys.stdout.write(next_char)
             sys.stdout.flush()
         sys.stdout.write('\n')
-
-    # train the model, output generated text after each iteration
-    generator = generate_training_data(text, char_indices)
-    for iteration in range(1, 1000):
-        print('-' * 50)
-        print('Iteration {}'.format(iteration))
-        model.reset_states()
-        i = 0
-        model.fit_generator(generator, samples_per_epoch=1024 * 1024 * 2, nb_epoch=1)
-
-        """
-        # Should work, but results in doubling each character ie. "aabbccddeeffgg..."
-        for X, y in generate_training_data(text, char_indices):
-            model.train_on_batch(X, y)
-            loss = model.test_on_batch(X, y)
-            i += 1
-            sys.stdout.write('\rProcessed {:.1f}\tLoss: {}\t\t'.format(100.0 * i / len(text), loss))
-            sys.stdout.flush()
-        sys.stdout.write('\n')
-        print 'X: ', array_to_str(X.reshape(X.shape[0], -1))
-        print 'y: ', array_to_str(y)
-        """
-
-        model.reset_states()
-        print_prediction(model, char_indices, indices_char)
         # Save model parameters
         model.save_weights('model.{}.iter{}.h5'.format(run_name, iteration), overwrite=True)
 
@@ -143,5 +150,5 @@ if __name__ == '__main__':
     run_name = sys.argv[2]
     text = read_text_from_file(filename)
     print('Text length {} characters'.format(len(text)))
-    main(run_name, text)
+    main(run_name, filename, text)
 
